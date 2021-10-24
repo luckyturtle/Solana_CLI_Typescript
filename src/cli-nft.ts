973d727e205de121b426689d8dec07340b9aba5f
PandaDevA <mianhua9999@gmail.com>
2021-10-24 13:29:36 +0300

    first commit

---
+++ src/cli-nft.ts
@@ -0,0 +1,67 @@
+import { program } from 'commander';
+import log from 'loglevel';
+import { mintNFT, updateNFTMetadata } from './commands/mint-nft';
+import { loadWalletKey } from './helpers/accounts';
+import { web3 } from '@project-serum/anchor';
+
+program.version('0.0.1');
+log.setLevel('info');
+
+programCommand('mint')
+  .option('-m, --metadata <string>', 'metadata url')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { keypair, env, metadata } = cmd.opts();
+    const solConnection = new web3.Connection(web3.clusterApiUrl(env));
+    const walletKeyPair = loadWalletKey(keypair);
+    await mintNFT(solConnection, walletKeyPair, metadata);
+  });
+
+programCommand('createNFT')
+.option('-m, --metadata <string>', 'metadata url')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+.action(async (directory, cmd) => {
+  const { keypair, env, metadata } = cmd.opts();
+  const solConnection = new web3.Connection(web3.clusterApiUrl(env));
+  const walletKeyPair = loadWalletKey(keypair);
+  await mintNFT(solConnection, walletKeyPair, metadata);
+});
+
+programCommand('updateNFTMetadata')
+.option('-m, --metadata <string>', 'metadata url')
+.option('-n, --nftAddress <string>', 'nft account address')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+.action(async (directory, cmd) => {
+  const { keypair, env, metadata, nftAddress } = cmd.opts();
+  const solConnection = new web3.Connection(web3.clusterApiUrl(env));
+  const walletKeyPair = loadWalletKey(keypair);
+  log.info(`=> Token Address: ${nftAddress}`);
+  await updateNFTMetadata(solConnection, walletKeyPair, metadata, nftAddress);
+});
+
+function programCommand(name: string) {
+  return program
+    .command(name)
+    .option(
+      '-e, --env <string>',
+      'Solana cluster env name',
+      'devnet', //mainnet-beta, testnet, devnet
+    )
+    .option(
+      '-k, --keypair <path>',
+      `Solana wallet location`,
+      '--keypair not provided',
+    )
+    .option('-l, --log-level <string>', 'log level', setLogLevel);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function setLogLevel(value, prev) {
+  if (value === undefined || value === null) {
+    return;
+  }
+  log.info('setting the log value to: ' + value);
+  log.setLevel(value);
+}
+
+program.parse(process.argv);
